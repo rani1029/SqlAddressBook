7236d932ef0050b2efc8e7addd9a2700efa26c3d
rani1029 <ranig1029@gmail.com>
2021-09-07 13:10:56 +0530

    Table created

--- SQLAddressBook.sql
+++ SQLAddressBook.sql
@@ -2,3 +2,18 @@
 
 
 CREATE DATABASE AddressBookService;
+
+--uc2
+USE AddressBookService;
+
+CREATE TABLE  AddressBookTable
+	( Id int identity(1, 1) primary Key,
+	FirstName varchar(40),
+	LastName varchar(40),
+	Address varchar(250),
+	City varchar(25),
+	State varchar(25),
+	Zipcode varchar(6),
+	PhoneNumber varchar(10),
+	email varchar(30)	
+	)
